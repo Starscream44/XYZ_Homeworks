c3277447f5bf315b7d510d3bbf16b754f6b57621
Egor Nikitin <Lato@bk.ru>
2025-10-18 18:43:01 +0300

    Add project files.

---
+++ ApplesGame/GameMain.cpp
@@ -0,0 +1,161 @@
+﻿// ©2023, XYZ School. All rights reserved.
+// Authored by Aleksandr Rybalka (polterageist@gmail.com)
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
+const std::string RESOURCES_PATH = "Resources/";
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+const float INITIAL_SPEED = 100.f; // Pixels per second
+const float PLAYER_SIZE = 20.f;
+const float ACCELERATION = 20.f; // Pixels per second
+const int NUM_APPLES = 20;
+const float APPLE_SIZE = 20.f;
+
+int main()
+{
+	int seed = (int)time(nullptr);
+	srand(seed);
+	// Init window
+	sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Apples game!");
+
+	// Init player state
+	float playerX = SCREEN_WIDTH / 2.f;
+	float playerY = SCREEN_HEIGHT / 2.f;
+	float playerSpeed = INITIAL_SPEED;
+	int playerDirection = 0; // 0 - Right, 1 - Up, 2 - Left, 3 - Down
+
+	// Init player shape
+	sf::RectangleShape playerShape;
+	playerShape.setSize(sf::Vector2f(PLAYER_SIZE, PLAYER_SIZE));
+	playerShape.setFillColor(sf::Color::Yellow);
+	playerShape.setOrigin(PLAYER_SIZE / 2.f, PLAYER_SIZE / 2.f);
+	playerShape.setPosition(playerX, playerY);
+
+	//init aples
+	float applesX[NUM_APPLES];
+	float applesY[NUM_APPLES];
+	bool isAppleEaten[NUM_APPLES];
+	sf::CircleShape appleShape[NUM_APPLES];
+
+	for (int i = 0; i < NUM_APPLES; ++i)
+	{
+		isAppleEaten[i] = false;
+		applesX[i] = rand() / (float)RAND_MAX * SCREEN_WIDTH;
+		applesY[i] = rand() / (float)RAND_MAX * SCREEN_HEIGHT;
+
+		appleShape[i].setRadius(APPLE_SIZE/2.f);
+		appleShape[i].setFillColor(sf::Color::Red);
+		appleShape[i].setOrigin(APPLE_SIZE / 2.f, APPLE_SIZE / 2.f);
+		appleShape[i].setPosition(applesX[i], applesY[i]);
+	}
+
+	int numEatenApples = 0;
+
+	// Init game clocks
+	sf::Clock gameClock;
+	float lastTime = gameClock.getElapsedTime().asSeconds();
+
+	// Main loop
+	while (window.isOpen())
+	{
+		// Calculate time delta
+		float currentTime = gameClock.getElapsedTime().asSeconds();
+		float deltaTime = currentTime - lastTime;
+		lastTime = currentTime;
+
+		// Read events
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		// Handle input
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) ||
+			sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+		{
+			playerDirection = 0;
+		}
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) ||
+			sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+		{
+			playerDirection = 1;
+		}
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) ||
+			sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+		{
+			playerDirection = 2;
+		}
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) ||
+			sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+		{
+			playerDirection = 3;
+		}
+
+		// Update player state
+		playerSpeed += ACCELERATION * deltaTime;
+
+		if (playerDirection == 0)
+		{
+			playerX += playerSpeed * deltaTime;
+		}
+		else if (playerDirection == 1)
+		{
+			playerY -= playerSpeed * deltaTime;
+		}
+		else if (playerDirection == 2)
+		{
+			playerX -= playerSpeed * deltaTime;
+		}
+		else if (playerDirection == 3)
+		{
+			playerY += playerSpeed * deltaTime;
+		}
+
+		// Check screen borders collision
+		if (playerX - PLAYER_SIZE / 2.f < 0.f || playerX + PLAYER_SIZE / 2.f > SCREEN_WIDTH ||
+			playerY - PLAYER_SIZE / 2.f < 0.f || playerY + PLAYER_SIZE / 2.f > SCREEN_HEIGHT)
+		{
+			window.close();
+			break;
+		}
+
+		for (int i = 0; i < NUM_APPLES; ++i)
+		{
+			if (!isAppleEaten[i])
+			{
+				float sqauredDistance = (playerX - applesX[i]) * (playerX - applesX[i]) +
+					(playerY - applesY[i]) * (playerY - applesY[i]);
+				float sqareRadiusSum = (APPLE_SIZE + PLAYER_SIZE) * (APPLE_SIZE + PLAYER_SIZE) / 4.f;
+				if (sqauredDistance < sqareRadiusSum)
+				{
+					isAppleEaten[i] = true;
+					numEatenApples++;
+				}
+			}
+		}
+
+		if(numEatenApples >= NUM_APPLES)
+		{
+			window.close();
+			break;
+		}
+
+		window.clear();
+		playerShape.setPosition(playerX, playerY);
+		for (int i = 0; i < NUM_APPLES; ++i)
+		{
+			if (!isAppleEaten[i])
+			{
+				window.draw(appleShape[i]);
+			}
+		}
+		window.draw(playerShape);
+		window.display();
+	}
+
+	return 0;
+}
